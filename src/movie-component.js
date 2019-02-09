b2ff49341ed3d348c6758af182320b7ebf6c7b50
castleArg <markchurcher@gmail.com>
2019-02-09 19:41:09 +1300

    handle null dates

--- src/movie-component.js
+++ src/movie-component.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 const MovieComponent = (props) => {
     //now we can set state in a functional component
     const [isExpanded, setIsExpanded] = useState(false);
-    const theDate = new Date(parseInt(props.movie.ReleaseDate.slice(6, -2))).toDateString();
+    const theDate = props.movie.ReleaseDate ?
+     new Date(parseInt(props.movie.ReleaseDate.slice(6, -2))).toDateString() 
+     : 'NO DATE RETURNED';
     return (<div>
-        <h3 onClick={() => setIsExpanded(!isExpanded)}>{props.movie.Name}</h3>
+        <h3 onClick={() => setIsExpanded(!isExpanded)}>{props.movie.Name}} </h3>
         {isExpanded && <ul>
             <li>Id: {props.movie.Id}</li>
             <li>Year: {props.movie.MovieYear}</li>
